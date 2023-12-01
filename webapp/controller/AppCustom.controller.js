375481708d8242459f6e9240f30b7ef62c31e4bf
Lautaro Capella <lautaro.capella@globant.com>
2023-12-01 18:32:40 -0300

    de-minified controllers

--- webapp/controller/AppCustom.controller.js
+++ webapp/controller/AppCustom.controller.js
@@ -4,149 +4,131 @@ sap.ui.define([
 	"sap/ui/Device",
 	"cross/fnd/fiori/inbox/util/DataManager",
 	"cross/fnd/fiori/inbox/util/StartupParameters"
-], function(B, J, D, a, S) {
+], function (BaseController /* B */, JSONModel /* J */, Device /* D */, DataManager /* a */,
+	StartupParameters /* S */) {
 	"use strict";
 	return sap.ui.controller("cross.fnd.fiori.inbox.CA_FIORI_INBOXExtension2.controller.AppCustom", {
-		    onInit: function () {
-		        var m = new J();
-		        this.setModel(m, "fcl");
-		        var M = new J();
-		        this.setModel(M, "parametersModel");
-		        var o = this.getOwnerComponent();
-		        var d = new a(this);
-		        console.log("#### App DataManager", d);
-		        console.log("#### App StartupParameters", S);
-		        o.setDataManager(d);
-		        this.oStartupParameters = S.getInstance();
-		        
-		        sap.ushell.services.AppConfiguration.setApplicationFullWidth(true);
-		        
-		        if (this.oStartupParameters.isModeActive()) {
-		            var v, O = this.getView().getBusyIndicatorDelay();
-		            v = new J({
-		                busy: true,
-		                delay: O
-		            });
-		            this.setModel(v, "appView");
-		            var e = this.getOwnerComponent().getErrorHandler();
-		            var s = function (I) {
-		                e.setIsMetadataLoadedFailed(I);
-		                v.setProperty("/busy", false);
-		                v.setProperty("/delay", O);
-		            };
-		            this.getOwnerComponent().getModel().metadataLoaded().then(s.bind(this, false));
-		            this.getOwnerComponent().getModel().attachMetadataFailed(s.bind(this, true));
-		            var i = e.getIsMetadataLoadedFailed();
-		            if (i) {
-		                s(i);
-		            }
-		        }
-		        console.log("#### App getContentDensityClass", this.getOwnerComponent().getContentDensityClass());
-		        this.getView().addStyleClass(this.getOwnerComponent().getContentDensityClass());
-		   //      console.log("#### App AdjustHeightWithShellBar", o.getDataManager().getAdjustHeightWithShellBar());
-		   //      var methods = [];
-					// for (var m in this.getView().byId("app")) {        
-					//     if (typeof this.getView().byId("app")[m] == "function") {
-					//         methods.push(m);
-					//     }
-					// }
-		   //      console.log("#### App methods", methods.join(","));
-				// console.log("#### App css class sapUShellApplicationContainer ", this.getView().byId("app").hasStyleClass("sapUShellApplicationContainer"));
-				// console.log("#### App css class sapUshellApplicationPage ", this.getView().byId("app").hasStyleClass("sapUshellApplicationPage"));
-				// console.log("#### App css class sapUshellDefaultBackground ", this.getView().byId("app").hasStyleClass("sapUshellDefaultBackground"));
-				// console.log("#### App css class sapUShellApplicationContainerLimitedWidth ", this.getView().byId("app").hasStyleClass("sapUShellApplicationContainerLimitedWidth"));
+		onInit: function () {
+			var oFclModel = new JSONModel();
+			this.setModel(oFclModel, "fcl");
+			var oParametersModel = new JSONModel();
+			this.setModel(oParametersModel, "parametersModel");
+			var oComponent = this.getOwnerComponent();
+			var oDataManager = new DataManager(this);
+			console.log("#### App DataManager", oDataManager);
+			console.log("#### App StartupParameters", StartupParameters);
+			oComponent.setDataManager(oDataManager);
+			this.oStartupParameters = StartupParameters.getInstance();
 
-				// this.getView().byId("app").removeStyleClass("sapUShellApplicationContainer");
-				// this.getView().byId("app").removeStyleClass("sapUshellApplicationPage");
-				// this.getView().byId("app").removeStyleClass("sapUshellDefaultBackground");
-				// this.getView().byId("app").removeStyleClass("sapUShellApplicationContainerLimitedWidth");
-				
-		        // if (o.getDataManager().getAdjustHeightWithShellBar()) {
-		        //     this.getView().byId("app").addStyleClass("adjustHeightWithShellBar");
-		        // }
-		        this.oRouter = this.getOwnerComponent().getRouter();
-		        this.oRouter.attachRouteMatched(this.onRouteMatched, this);
-		        this.oRouter.attachBeforeRouteMatched(this.onBeforeRouteMatched, this);
-		        
-		        sap.ushell.services.AppConfiguration.setApplicationFullWidth(true);
-		    },
-		    onAfterRendering: function(e){
-		  //  		console.log("#### App onAfterRendering css class sapUShellApplicationContainer ", this.getView().byId("app").hasStyleClass("sapUShellApplicationContainer"));
-				// console.log("#### App css onAfterRendering class sapUshellApplicationPage ", this.getView().byId("app").hasStyleClass("sapUshellApplicationPage"));
-				// console.log("#### App css onAfterRendering class sapUshellDefaultBackground ", this.getView().byId("app").hasStyleClass("sapUshellDefaultBackground"));
-				// console.log("#### App css onAfterRendering class sapUShellApplicationContainerLimitedWidth ", this.getView().byId("app").hasStyleClass("sapUShellApplicationContainerLimitedWidth"));
+			sap.ushell.services.AppConfiguration.setApplicationFullWidth(true);
 
-				// this.getView().byId("app").removeStyleClass("sapUShellApplicationContainer");
-				// this.getView().byId("app").removeStyleClass("sapUshellApplicationPage");
-				// this.getView().byId("app").removeStyleClass("sapUshellDefaultBackground");
-				// this.getView().byId("app").removeStyleClass("sapUShellApplicationContainerLimitedWidth");
-				sap.ushell.services.AppConfiguration.setApplicationFullWidth(true);
-		    },
-		    onColumnResize: function (e) {
-		    	sap.ushell.services.AppConfiguration.setApplicationFullWidth(true);
-		        var m = e.getSource().getBeginColumnPages()[0];
-		        if (m && typeof m.getController().iIndex === "number" && e.getParameter("beginColumn")) {
-		            var t = m.byId("table");
-		            t.$().is(":visible") && t.scrollToIndex(m.getController().iIndex);
-		        }
-		    },
-		    onBeforeRouteMatched: function (e) {
-		    	sap.ushell.services.AppConfiguration.setApplicationFullWidth(true);
-		        var m = this.getModel("fcl");
-		        var l = e.getParameters().arguments.layout;
-		        if (!l || !this.oStartupParameters.isFlexibleColumnLayout()) {
-		            var n = this.getOwnerComponent().getFCLHelper().getNextUIState(0);
-		            l = n.layout;
-		        }
-		        if (l) {
-		            m.setProperty("/layout", l);
-		        }
-		    },
-		    onRouteMatched: function (e) {
-		    	sap.ushell.services.AppConfiguration.setApplicationFullWidth(true);
-		        var r = e.getParameter("name");
-		        var A = e.getParameter("arguments");
-		        this._updateUIElements();
-		        this.currentRouteName = r;
-		        this.SAP__Origin = decodeURIComponent(A.SAP__Origin);
-		        this.InstanceID = decodeURIComponent(A.InstanceID);
-		    },
-		    onStateChanged: function (e) {
-		    	sap.ushell.services.AppConfiguration.setApplicationFullWidth(true);
-		        var i = e.getParameter("isNavigationArrow");
-		        var l = e.getParameter("layout");
-		        var r = !D.system.phone;
-		        this._updateUIElements();
-		        if (i) {
-		            if (l === "ThreeColumnsMidExpandedEndHidden") {
-		                this.getModel("parametersModel").setProperty("/showLogButtonPressed", false);
-		                this.getModel("parametersModel").setProperty("/showDetailsButtonPressed", false);
-		            }
-		            this.oRouter.navTo(this.currentRouteName, {
-		                SAP__Origin: encodeURIComponent(this.SAP__Origin),
-		                InstanceID: encodeURIComponent(this.InstanceID),
-		                layout: l
-		            }, { dummyProperty: "dummyValue" }, r);
-		        }
-		        if (l === "ThreeColumnsMidExpanded") {
-		            if (this.currentRouteName === "myTasksDetailDetail") {
-		                this.getModel("parametersModel").setProperty("/showLogButtonPressed", true);
-		            }
-		            if (this.currentRouteName === "additionalTaskDetails") {
-		                this.getModel("parametersModel").setProperty("/showDetailsButtonPressed", true);
-		            }
-		        }
-		    },
-		    _updateUIElements: function () {
-		    	sap.ushell.services.AppConfiguration.setApplicationFullWidth(true);
-		        var m = this.getModel("fcl");
-		        var u = this.getOwnerComponent().getFCLHelper().getCurrentUIState();
-		        m.setData(u);
-		    },
-		    onExit: function () {
-		        this.oRouter.detachRouteMatched(this.onRouteMatched, this);
-		        this.oRouter.detachBeforeRouteMatched(this.onBeforeRouteMatched, this);
-		        this.oStartupParameters.destroy();
-		    }
+			if (this.oStartupParameters.isModeActive()) {
+				var oAppViewModel;
+				var iDelay = this.getView().getBusyIndicatorDelay();
+				oAppViewModel = new JSONModel({
+					busy: true,
+					delay: iDelay
+				});
+				this.setModel(oAppViewModel, "appView");
+				var oErrorHandler = this.getOwnerComponent().getErrorHandler();
+				var fHandleMetadataLoad = function (bErrorOnMetadataLoad) {
+					oErrorHandler.setIsMetadataLoadedFailed(bErrorOnMetadataLoad);
+					oAppViewModel.setProperty("/busy", false);
+					oAppViewModel.setProperty("/delay", iDelay);
+				};
+				this.getOwnerComponent().getModel().metadataLoaded().then(fHandleMetadataLoad.bind(this, false));
+				this.getOwnerComponent().getModel().attachMetadataFailed(fHandleMetadataLoad.bind(this, true));
+				var bIsMetadataLoadedFailed = oErrorHandler.getIsMetadataLoadedFailed();
+				if (bIsMetadataLoadedFailed) {
+					fHandleMetadataLoad(bIsMetadataLoadedFailed);
+				}
+			}
+			console.log("#### App getContentDensityClass", this.getOwnerComponent().getContentDensityClass());
+			this.getView().addStyleClass(this.getOwnerComponent().getContentDensityClass());
+
+			this.oRouter = this.getOwnerComponent().getRouter();
+			this.oRouter.attachRouteMatched(this.onRouteMatched, this);
+			this.oRouter.attachBeforeRouteMatched(this.onBeforeRouteMatched, this);
+
+			sap.ushell.services.AppConfiguration.setApplicationFullWidth(true);
+		},
+
+		onAfterRendering: function (e) {
+			sap.ushell.services.AppConfiguration.setApplicationFullWidth(true);
+		},
+
+		onColumnResize: function (e) {
+			sap.ushell.services.AppConfiguration.setApplicationFullWidth(true);
+			var oBeginColumnPage = e.getSource().getBeginColumnPages()[0];
+			if (oBeginColumnPage
+				&& typeof oBeginColumnPage.getController().iIndex === "number"
+				&& e.getParameter("beginColumn")) {
+				var oTable = oBeginColumnPage.byId("table");
+				oTable.$().is(":visible") && oTable.scrollToIndex(oBeginColumnPage.getController().iIndex);
+			}
+		},
+
+		onBeforeRouteMatched: function (e) {
+			sap.ushell.services.AppConfiguration.setApplicationFullWidth(true);
+			var oFclModel = this.getModel("fcl");
+			var sLayout = e.getParameters().arguments.layout;
+			if (!sLayout || !this.oStartupParameters.isFlexibleColumnLayout()) {
+				var oNextUiState = this.getOwnerComponent().getFCLHelper().getNextUIState(0);
+				sLayout = oNextUiState.layout;
+			}
+			if (sLayout) {
+				oFclModel.setProperty("/layout", sLayout);
+			}
+		},
+
+		onRouteMatched: function (e) {
+			sap.ushell.services.AppConfiguration.setApplicationFullWidth(true);
+			var sName = e.getParameter("name");
+			var oArgs = e.getParameter("arguments");
+			this._updateUIElements();
+			this.currentRouteName = sName;
+			this.SAP__Origin = decodeURIComponent(oArgs.SAP__Origin);
+			this.InstanceID = decodeURIComponent(oArgs.InstanceID);
+		},
+
+		onStateChanged: function (e) {
+			sap.ushell.services.AppConfiguration.setApplicationFullWidth(true);
+			var bIsNavigationArrow = e.getParameter("isNavigationArrow");
+			var sLayout = e.getParameter("layout");
+			var bIsNotPhone = !Device.system.phone;
+			this._updateUIElements();
+			if (bIsNavigationArrow) {
+				if (sLayout === "ThreeColumnsMidExpandedEndHidden") {
+					this.getModel("parametersModel").setProperty("/showLogButtonPressed", false);
+					this.getModel("parametersModel").setProperty("/showDetailsButtonPressed", false);
+				}
+				this.oRouter.navTo(this.currentRouteName, {
+					SAP__Origin: encodeURIComponent(this.SAP__Origin),
+					InstanceID: encodeURIComponent(this.InstanceID),
+					layout: sLayout
+				}, { dummyProperty: "dummyValue" }, bIsNotPhone);
+			}
+			if (sLayout === "ThreeColumnsMidExpanded") {
+				if (this.currentRouteName === "myTasksDetailDetail") {
+					this.getModel("parametersModel").setProperty("/showLogButtonPressed", true);
+				}
+				if (this.currentRouteName === "additionalTaskDetails") {
+					this.getModel("parametersModel").setProperty("/showDetailsButtonPressed", true);
+				}
+			}
+		},
+
+		_updateUIElements: function () {
+			sap.ushell.services.AppConfiguration.setApplicationFullWidth(true);
+			var oFclModel = this.getModel("fcl");
+			var oCurrentUIState = this.getOwnerComponent().getFCLHelper().getCurrentUIState();
+			oFclModel.setData(oCurrentUIState);
+		},
+
+		onExit: function () {
+			this.oRouter.detachRouteMatched(this.onRouteMatched, this);
+			this.oRouter.detachBeforeRouteMatched(this.onBeforeRouteMatched, this);
+			this.oStartupParameters.destroy();
+		}
 	});
 });

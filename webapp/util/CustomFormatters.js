8e7cde172c3510f4fbc5ac71d0531f96238df385
Lautaro Capella <lautaro.capella@globant.com>
2023-12-20 13:25:43 -0300

    refactor formatterCustomAttributeValue method

--- webapp/util/CustomFormatters.js
+++ webapp/util/CustomFormatters.js
@@ -4,27 +4,15 @@ sap.ui.define([
 	"use strict";
 	// Module to contain all data comparators
 	UI5Object.extend("cross.fnd.fiori.inbox.CA_FIORI_INBOXExtension2.CustomFormatters", {});
-	
+
 	cross.fnd.fiori.inbox.CA_FIORI_INBOXExtension2.CustomFormatters = {
-		formatterCustomAttributeValue: function(sAttributeName, oCustomAttributeData) {
-			var oValue = null;
-			console.log("## formatterCustomAttributeValue", sAttributeName, oCustomAttributeData);
-			if(oCustomAttributeData){
-				for (var i=0; i<oCustomAttributeData.results.length; i++) {
-					console.log("## formatterCustomAttributeValue IF", oCustomAttributeData.results[i].Name.toLowerCase(), sAttributeName.toLowerCase());
-					//var oCustomAttribute = this.getModel().getProperty("/" + oCustomAttributeData[i]);
-					if (oCustomAttributeData.results[i].Name.toLowerCase() === sAttributeName.toLowerCase()) {
-						oValue = oCustomAttributeData.results[i].Value;
-						break;
-					}
-				}	
-			}
-			console.log("## formatterCustomAttributeValue", sAttributeName, oValue);
-			
-			return oValue;
+		formatterCustomAttributeValue: function (sAttributeName, oCustomAttributeData) {
+			return oCustomAttributeData
+				?.results
+				?.find(oCustomAttribute => oCustomAttribute.Name.toLowerCase() === sAttributeName.toLowerCase())
+				?.Value;
 		}
 	};
 
 	return cross.fnd.fiori.inbox.CA_FIORI_INBOXExtension2.CustomFormatters;
 });
-		

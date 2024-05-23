cb746235f3d98f35f6af17b0d2314a09e6e8c6ac
jegiraldoc <ext_jhgirald@mercadolibre.com.co>
2024-05-23 22:46:14 +0000

    Migration WebIDE To BAS

--- webapp/view/S2_TaskListCustom.controller.js
+++ webapp/view/S2_TaskListCustom.controller.js
@@ -853,6 +853,7 @@ sap.ui.define([
 		},
 
 		getProviderSystem: function(callback) {
+			//Get Model
 			let oModel = this.getOwnerComponent().getModel();
 
 			if (this.getOwnerComponent().getModel("ProviderSystem")){
